0e8eeb65f84d588900ba31e5ddaed6d1a0551ecf
MaiconJordan <maiconplus@gmail.com>
2024-10-22 21:03:28 -0300

    http response

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def car_view(request):
+    return HttpResponse('Meus Carrpos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', car_view),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
