dcac7790e255021d2070445e274992a1bec86b06
MaiconJordan <maiconplus@gmail.com>
2024-10-23 20:18:41 -0300

    cars_views

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
+from cars.views import car_view
 
-def car_view(request):
-    return HttpResponse('Meus Carrpos')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
